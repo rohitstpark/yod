d1067758209d262a7f69cbb91e8b4b347da98c49
Rohit Rajput <rohit.tspark@gmail.com>
2022-07-20 14:24:23 +0530

    chaneson shared module

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SidePanelComponent } from './side-panel/side-panel.component';
+
+@NgModule({
+  declarations: [
+    // TodoListComponent
+  ],
+  imports: [
+    CommonModule,
+    SidePanelComponent
+  ]
+})
+export class TodoModule { }
